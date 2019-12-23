c9356313b6465b1423510833e96738886f654b15
Phantsure <samparksharma2000@gmail.com>
2019-12-24 00:02:23 +0530

    dockerfile updated and minor updates

---
+++ shell/q10-worst-time-complexity/binary.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	long long ch;
+	cout << "Say the 4 digit magic number and thou shalt be rewarded!\n";
+	cin >> ch ;
+	if(ch == 6969)
+		cout << "Congratulations! The password is GLUG{c5_15_5h17}\n";
+	else
+		cout << "Too bad! Try again.\n";
+	return 0;
+}
