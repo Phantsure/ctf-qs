a06b45886f808b54a3654598c12bcf6bbfdbfb62
himanshu272 <himanshu.shekharjha.272@gmail.com>
2019-12-26 18:54:47 +0530

    add q1 in web

--- web/q1-GET_me_if_you_can/secret.php
+++ web/q1-GET_me_if_you_can/secret.php
@@ -8,11 +8,19 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
 <body>
-    <div class="jumbotron">
-        <form action="./secret.php" method="get">
-            <input name="auth" value="false" hidden>
-            <button class="btn btn-success">CLICK ME TO GET THE FLAG!</button>
-        </form>
-    </div>
+<div class="jumbotron">
+	<?php
+		if($_GET["auth"]=='false')
+		{
+			echo "<div class='alert alert-danger'>
+				No! I won't give you the flag. Bad user!
+			      </div>";
+		} else {
+			echo "<div class='alert alert-success'>
+				Fine! Here's the flag GLUG{5n0wd3n_3471n6_53cur17y}
+			      </div>";
+		}
+	?>
+</div>
 </body>
 </html>
